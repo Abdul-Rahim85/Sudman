4ec5786b7266ae0b2851a627af9bb9b20dd04c20
Abdulrahim <abdelrheem2204@gmail.com>
2024-10-10 00:23:28 -0700

    Adding .env file

--- routes/api/apiRoute.js
+++ routes/api/apiRoute.js
@@ -6,7 +6,7 @@ const apiRoute = express.Router();
 
 apiRoute.post('/singup', apiController.singup_post); //This route is use to send the sining in data
 apiRoute.post('/login', apiController.login_post); //This route is use to send the username and password to check them
-apiRoute.get('/detectdevice/:id', requireAuth, apiController.detectdevice_get); //This route is use to detect a device data
+apiRoute.get('/detectdevice/:id', apiController.detectdevice_get); //This route is use to detect a device data
 apiRoute.put('/user/:id', requireAuth, apiController.user_put); //This route is use to update a user data
 
 module.exports = apiRoute;
